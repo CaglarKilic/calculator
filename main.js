de90e754ff54e7473e0593a38ddd7028382c2bd2
Caglar Kilic <73300418+CaglarKilic@users.noreply.github.com>
2023-10-04 08:16:50 +0300

    Finished number and operator entry

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const PROMPT = document.querySelector('p')
 const MAX_DIGIT = 9
+let buffer = ''
+let opBuffer = []
+let numBuffer = []
 
 function init() {
     // ------- Buttons -------
@@ -11,12 +14,26 @@ function init() {
 
     //decimal
     document.querySelector('#decimal').addEventListener('click', addDecimal)
+
+    //operators
+    document.querySelector('#add').addEventListener('click', operate)
+    document.querySelector('#divide').addEventListener('click', operate)
+    document.querySelector('#subtract').addEventListener('click', operate)
+    document.querySelector('#multiply').addEventListener('click', operate)
 }
 
 // UTILITY
 
-function readInput() {
-    return PROMPT.textContent
+function read() {
+    return buffer || '0'
+}
+
+function write(input) {
+    PROMPT.textContent = input
+}
+
+function flush() {
+    buffer = ''
 }
 
 function countDigits(number, part = 'all') {
@@ -24,26 +41,57 @@ function countDigits(number, part = 'all') {
     return number.match(/\d/g).length
 }
 
-
-// INPUT
-
-function addDigit() {
-    let input = readInput()
-    if (countDigits(input) == MAX_DIGIT) { return }
-    writeInput(input + this.textContent)
+function parseInput(input) {
+    return parseFloat(input.match(/[^,]/g).join(''))
 }
 
-function addDecimal() {
-    let input = readInput()
-    if (countDigits(input) == MAX_DIGIT || input.includes('.')) { return }
-    PROMPT.textContent = input + this.textContent
+function clearInput() {
+    if (this.textContent) { }
+    writeInput('0')
 }
 
 function writeInput(input) {
     let options = countDigits(input) > MAX_DIGIT ? { notation: 'scientific' } : {}
-    let float = parseFloat(input.match(/[^,]/g).join(''))
+    let float = parseInput(input)
     if (input.includes('.') && !input.includes('e')) { float = float.toFixed(countDigits(input, 'decimal')) }
-    PROMPT.textContent = float.toLocaleString('en-US', options)
+    buffer = float.toLocaleString('en-US', options)
+}
+
+function registerBuffers(op) {
+    if (buffer) {
+        numBuffer.push(parseInput(buffer))
+        flush()
+    }
+
+    if (numBuffer.length > opBuffer.length) { opBuffer.push(op) }
+    else {
+        opBuffer.pop()
+        opBuffer.push(op)
+    }
+}
+
+
+// INPUT
+
+function addDigit() {
+    let input = read()
+    if (countDigits(input) == MAX_DIGIT) { return }
+    writeInput(input + this.textContent)
+    write(buffer)
+}
+
+function addDecimal() {
+    let input = read()
+    if (countDigits(input) == MAX_DIGIT || input.includes('.')) { return }
+    buffer = input + this.textContent
+    write(buffer)
+}
+
+
+// OPERATION
+
+function operate() {
+    registerBuffers(this.textContent)
 }
 
 
